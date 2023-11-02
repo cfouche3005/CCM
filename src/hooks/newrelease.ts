f0f106f8c880b3cbab0f5510ec145343580b15e6
cfouche <cfouche3005@gmail.com>
2023-11-02 11:02:56 +0000

    add logging

--- src/hooks/newrelease.ts
+++ src/hooks/newrelease.ts
@@ -1,6 +1,7 @@
 import { Elysia } from "elysia";
 import PocketBase from 'pocketbase';
 import { requestLogger } from "../log/log";
+import { TypedPocketBase } from "../types/pocketbase.types";
 
 
 
@@ -24,25 +25,30 @@ interface nftyJSON {
 }
 
 function basicAuthParser(authHeader :string){
+    console.log("beginning auth parsing ",authHeader)
     const authArray: string[] = Buffer.from(authHeader.split(' ')[1],'base64').toString().split(':');
     const authData: authData = {
         user : authArray[0],
         password : authArray[1]
     }
+    console.log("auth parsed ",authData)
     return authData
 }
 
 async function getPBdata(auth :authData,project :string) {
-    const pb = new PocketBase('http://127.0.0.1:' + (process.env.PB_PORT || 8090));
+    console.log("beginning pb data fetching")
+    const pb = new PocketBase('http://127.0.0.1:' + (process.env.PB_PORT || 8090)) as TypedPocketBase;
     const pbAuth = await pb.collection('users').authWithPassword(auth.user,auth.password);
     const software = await pb.collection('monitored_software').getFirstListItem(`project="${project}"`, {
         expand: "provider"
     })
     pb.authStore.clear();
+    console.log("pb data fetched ",software)
     return software
 }
 
 function nftyParser(pocketbasedata: any,newreleasedata: any){
+    console.log("beginning nfty parsing")
     const ntfy: nftyJSON = {
         topic : pocketbasedata.topic,
         title : pocketbasedata.name + " " + newreleasedata.version,
@@ -58,10 +64,12 @@ function nftyParser(pocketbasedata: any,newreleasedata: any){
             url : pocketbasedata.expand.provider.changelog_url[0]+pocketbasedata.project+pocketbasedata.expand.provider.changelog_url[1]+newreleasedata.version
         }].concat(pocketbasedata.actions)
     }
+    console.log("nfty parsed ",ntfy)
     return ntfy;
 }
 
 function sendToNtfy(ntfyBody :nftyJSON) {
+    console.log("beginning nfty sending")
     fetch('https://'+ process.env.NTFY_DOMAIN || "ntfy.sh",{
         method: "POST",
         body: JSON.stringify(ntfyBody),
@@ -69,6 +77,7 @@ function sendToNtfy(ntfyBody :nftyJSON) {
             Authorization: "Bearer " + process.env.NTFY_APIKEY || ""
         }
     })
+    console.log("nfty sent")
 }
 
 const newrelease = new Elysia()
